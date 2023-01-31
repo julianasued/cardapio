04cb5ff1e0475f7f83b8dda881b210ec4770f3b0
julianasued <julianasuedprof@gmail.com>
2023-01-31 20:56:15 -0300

    feat: buscador pronto

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,8 +1,10 @@
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from 'pages/Buscador';
+import { useState } from 'react';
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -17,7 +19,7 @@ export default function Cardapio() {
                 <h3 className={styles.cardapio__titulo}>
                     Cardápio
                 </h3>
-                <Buscador />
+                <Buscador busca={busca} setBusca={setBusca} />
             </section>
         </main>
     );
