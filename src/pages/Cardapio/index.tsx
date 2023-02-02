ffc5386a371d5d292b420d81f58c3db05d161138
julianasued <julianasuedprof@gmail.com>
2023-02-02 18:33:29 -0300

    criando os filtros

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,8 @@
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
-import Buscador from 'pages/Buscador';
+import Buscador from 'pages/Cardapio/Buscador';
 import { useState } from 'react';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
@@ -20,6 +21,9 @@ export default function Cardapio() {
                     Cardápio
                 </h3>
                 <Buscador busca={busca} setBusca={setBusca} />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros />
+                </div>
             </section>
         </main>
     );
